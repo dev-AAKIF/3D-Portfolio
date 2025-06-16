6c946f5ea3580342ae9f46aaa85e023ec5f15e69
Akif <akif7868@gmail.com>
2025-06-16 21:45:42 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -107,6 +107,11 @@ const Home = () => {
         />
       
       </div>
+      
+
+      <div className='absolute bottom-4 right-4 bg-white/80 backdrop-blur-md px-4 py-2 rounded-xl shadow-lg border border-gray-300 text-sm sm:text-base text-black z-50'>
+        Use keyboard navigation or scroll to see the magic ✨
+      </div>
     </section>
   )
 }
